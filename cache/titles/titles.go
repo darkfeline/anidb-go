c2c6f443cc3a13ec9e05afc64a538eb3006bbbf2
Allen Li <darkfeline@felesatra.moe>
2018-06-18 22:03:25 -0700

    Add cache

---
+++ cache/titles/titles.go
@@ -0,0 +1,62 @@
+// Copyright (C) 2018 Allen Li
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package titles provides a cache for AniDB titles data.
+//
+// Documentation for the AniDB APIs can be found at
+// https://wiki.anidb.net/w/API.
+package titles
+
+import (
+	"encoding/gob"
+	"os"
+	"path/filepath"
+
+	"go.felesatra.moe/anidb"
+	"go.felesatra.moe/xdg"
+)
+
+func Load(path string) ([]anidb.Anime, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	d := gob.NewDecoder(f)
+	var a []anidb.Anime
+	err = d.Decode(&a)
+	if err != nil {
+		return nil, err
+	}
+	return a, nil
+}
+
+var cacheDir = filepath.Join(xdg.CacheHome(), "go.felesatra.moe_anidb")
+var titlesPath = filepath.Join(cacheDir, "titles.gob")
+
+func LoadDefault() ([]anidb.Anime, error) {
+	return Load(titlesPath)
+}
+
+func Save(path string, a []anidb.Anime) error {
+	f, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+	e := gob.NewEncoder(f)
+	return e.Encode(a)
+}
+
+func SaveDefault(a []anidb.Anime) error {
+	return Save(titlesPath, a)
+}
---
+++ cache/titles/titles_test.go
@@ -0,0 +1,57 @@
+// Copyright (C) 2018 Allen Li
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package titles
+
+import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"reflect"
+	"testing"
+
+	"go.felesatra.moe/anidb"
+)
+
+func TestSaveAndLoad(t *testing.T) {
+	d, err := ioutil.TempDir("", "test")
+	if err != nil {
+		t.Fatalf("Error creating temporary dir: %s", err)
+	}
+	defer os.RemoveAll(d)
+	a := []anidb.Anime{{AID: 22, Titles: []anidb.Title{
+		{
+			Name: "Neon Genesis Evangelion",
+			Type: "official",
+			Lang: "en",
+		},
+		{
+			Name: "Shinseiki Evangelion",
+			Type: "main",
+			Lang: "x-jat",
+		},
+	}}}
+	f := filepath.Join(d, "foo.gob")
+	err = Save(f, a)
+	if err != nil {
+		t.Fatalf("Error saving: %s", err)
+	}
+	got, err := Load(f)
+	if err != nil {
+		t.Errorf("Error loading: %s", err)
+	}
+	if !reflect.DeepEqual(got, a) {
+		t.Errorf("Expected %#v, got %#v", a, got)
+	}
+}
