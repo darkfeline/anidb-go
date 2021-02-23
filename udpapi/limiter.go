51437b8fe93609b7c7d4e339e52e434ba7e5fabb
Allen Li <darkfeline@felesatra.moe>
2021-02-22 23:34:44 -0800

    Add limiter

---
+++ udpapi/limiter.go
@@ -0,0 +1,48 @@
+// Copyright (C) 2021 Allen Li
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package udpapi
+
+import (
+	"context"
+
+	"golang.org/x/time/rate"
+)
+
+// A Limiter is a rate limiter that complies with AniDB UDP API flooe
+// prevention recommendations.
+type Limiter struct {
+	short *rate.Limiter
+	long  *rate.Limiter
+}
+
+// NewLimiter returns a new Limiter.
+func NewLimiter() *Limiter {
+	return &Limiter{
+		// Every 2 sec short term
+		short: rate.NewLimiter(0.5, 1),
+		// Every 4 sec long term after 60 seconds
+		long: rate.NewLimiter(0.25, 60/2),
+	}
+}
+
+func (l Limiter) Wait(ctx context.Context) error {
+	if err := l.long.Wait(ctx); err != nil {
+		return err
+	}
+	if err := l.short.Wait(ctx); err != nil {
+		return err
+	}
+	return nil
+}
